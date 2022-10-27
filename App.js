a68fc87537d7ed917a56bcce260810010fdef33d
Ben Fisk <bfisk1711@gmail.com>
2022-10-27 17:00:17 -0500

    Start navigation and add some packages.

--- App.js
+++ App.js
@@ -1,5 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 export default function App() {
   return (
