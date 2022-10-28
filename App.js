d399081f66386e1fe45118c229993dbbc36cda80
Matthew Freestone <matthewfreestone1593@gmail.com>
2022-10-28 10:41:55 -0500

    Added basic navigation

--- App.js
+++ App.js
@@ -7,22 +7,76 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+
 export default function App() {
+  const Stack = createNativeStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-      <CometButton onPress={() => Alert.alert("Button Clicked")}>Click me</CometButton>
-      <ComentInput />
+    <NavigationContainer>
+      <Stack.Navigator style={styles.container}>
+          <Stack.Screen name="Home" component={HomeScreen} /> 
+          <Stack.Screen name="Sign In" component={SignIn} />
+          <Stack.Screen name="Check In" component={CheckIn} />
+      </Stack.Navigator>
+      <View style={[styles.container,styles.blueBG]}>
+        <Text>This is part of the App component.</Text>
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
+  );
+}
+function HomeScreen ({ navigation }) {
+  return (
+    <View style={[styles.container, styles.redBG]}>
+      <Text style={styles.text}>This is the Home screen of the app</Text>
+      <CometButton onPress={() => navigation.navigate('Sign In')}>Sign In</CometButton>
+      <CometButton onPress={() => navigation.navigate('Check In')}>Check In</CometButton>
     </View>
   );
 }
+function SignIn( { navigation } ) {
+  return (
+    <View style={[styles.container, styles.yellowBG]}>
+      <Text style={styles.text}>This is the SignIn screen of the app</Text>
+      <CometButton onPress={() => navigation.navigate('Home')}>Home</CometButton>
+      <CometButton onPress={() => navigation.navigate('Check In')}>Check In</CometButton>
+    </View>
+  )
+}
+function CheckIn( { navigation } ) {
+  return (
+    <View style={[styles.container, styles.greenBG]}>
+      <Text style={styles.text}>This is the CheckIn screen of the app</Text>
+      <CometButton onPress={() => navigation.navigate('Home')}>Home</CometButton>
+      <CometButton onPress={() => navigation.navigate('Sign In')}>SignIn</CometButton>
+    </View>
+  )
+}
+
 
 const styles = StyleSheet.create({
+  text : {
+    color: 'black',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
   container: {
     flex: 1,
-    backgroundColor: '#f5f5f5',
+    width: '100%',
     alignItems: 'center',
     justifyContent: 'center',
+    gap: 10,
+  },
+  redBG: {
+    backgroundColor: '#ff000080',
+  },
+  blueBG: {
+    backgroundColor: '#0000ff80',
+  },
+  greenBG: {
+    backgroundColor: '#00ff0080',
+  },
+  yellowBG: {
+    backgroundColor: '#ffff0080',
   },
 });
--- pages/Sign_In.js
+++ pages/Sign_In.js
@@ -1,14 +1,18 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Image } from 'react-native'
 import React from 'react'
 
-export default function Sign_In() {
+export default function Sign_In( { navigation } ) {
   return (
     <View>
+      <Image source={require('../assets/Logo.svg')} style={styles.logo} />
       <Text>Sign_In</Text>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-    
+  logo: {
+    width: 100,
+    height: 100,
+  },
 })
