1bd97fc98ca7333db9cd07d1572e9ec59faa46d0
Ben Fisk <bfisk1711@gmail.com>
2022-10-30 20:00:22 -0500

    Add reanimated requirement

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Alert } from 'react-native';
 import CometButton from './components/CometButton';
-import ComentInput from './components/CometInput';
+import CometInput from './components/CometInput';
 import React from 'react';
 
 import { NavigationContainer } from '@react-navigation/native';
@@ -13,7 +13,7 @@ export default function App() {
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
       <CometButton onPress={() => Alert.alert("Button Clicked")}>Click me</CometButton>
-      <ComentInput />
+      <CometInput />
     </View>
   );
 }
