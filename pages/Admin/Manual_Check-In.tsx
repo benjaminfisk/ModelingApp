d0fbf6bf52f8a7f26cfc93ccd48429b3ebe50664
Matthew Freestone <matthewfreestone1593@gmail.com>
2022-11-01 11:19:52 -0500

    Finished Check-In Page format

--- pages/Admin/Manual_Check-In.tsx
+++ pages/Admin/Manual_Check-In.tsx
@@ -1,25 +1,58 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 
-import { global_styles } from '../../assets/styles'
+import { global_styles, colors } from '../../assets/styles'
 import Header from '../../components/Header'
+import CometButton from '../../components/CometButton'
+import CometInput from '../../components/CometInput'
+import Checkbox from '../../components/Checkbox'
 
 type Props = {
   navigation: any
 }
 
 const Manual_Check_In: React.FC<Props> = (props) => {
-  const { navigation } = props
+  const { navigation } = props;
   return (
-    <View style={global_styles.container}>
-      <Header title="MANUAL CHECK-IN"/>
-      <Text style={global_styles.pageTitle}>MANUAL CHECK-IN</Text>
+    <View style={[global_styles.container, {backgroundColor: colors.accent}]}>
+      <Header title='MANUAL CHECK-IN' />
+      <View style={styles.inputs}>
+        <Text style={global_styles.pageTitle}>INPUT INFORMATION</Text>
+        <View style={styles.spacer}/>
+        <CometInput label='Student Name'/>
+        <View style={styles.spacer}/>
+        <CometInput label='Student ID'/>
+        <View style={styles.spacer}/>
+        <CometInput label='Current Time'/>
+        <View style={styles.spacer}/>
+        <Checkbox label='I verify that this student attended practice.'/>
+      </View>
+      
+      <View style={styles.buttons}>
+        <CometButton>Submit</CometButton>
+      </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-    
+  buttons: {
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 64,
+  },
+  inputs: {
+    marginLeft: 45,
+    marginRight: 45,
+    marginTop: 64,
+    alignContent: 'center',
+  },
+  spacer: {
+    height: 32,
+  },
 })
 
 export default Manual_Check_In
