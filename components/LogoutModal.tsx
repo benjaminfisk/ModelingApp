05f2728eee81b9a98b4844c3407b4ccc119b4e94
Ben Fisk <bfisk1711@gmail.com>
2022-11-01 13:24:30 -0500

    Start building drawer.

---
+++ components/LogoutModal.tsx
@@ -0,0 +1,14 @@
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
+
+export default function LogoutModal() {
+  return (
+    <View>
+      <Text>LogoutModal</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    
+})
