0d381ae0a352a4b948aa1f5cea3bf53572dc3cf8
Matthew Freestone <matthewfreestone1593@gmail.com>
2022-11-03 11:04:57 -0500

    finished check-in screen

--- pages/AUMB_CheckIn.tsx
+++ pages/AUMB_CheckIn.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
-import { global_styles } from '../assets/styles'
+import { View, Text, StyleSheet, Image } from 'react-native'
+import { colors, global_styles } from '../assets/styles'
 import CometButton from '../components/CometButton'
+import Header from '../components/Header'
+import mapImg from '../assets/map.png'
 
 type Props = {
   navigation: any
@@ -11,10 +13,40 @@ const AUMB_CheckIn: React.FC<Props> = (props) => {
   const { navigation } = props;
   return (
     <View style={global_styles.container}>
-      <Text style={global_styles.pageTitle}>This is the Check In screen of the app</Text>
-      {/* <CometButton onPress={() => navigation.navigate('Home')}>Home</CometButton> */}
-      <CometButton onPress={() => navigation.navigate('Sign In')}>Sign In</CometButton>
+      <Header title="AUMB CHECK-IN"/>
+      <View style={styles.titleView}>
+        <Text style={global_styles.pageTitle}>PRACTICE LOCATION</Text>
+      </View>
+      <Image source={mapImg} style={styles.mapImg} />
+      <View style={styles.textView}>
+        <Text style={{fontWeight: '500', fontSize: 16, color: colors.primary, marginVertical: 8}}>Band Practice Field</Text>
+        <Text style={{fontWeight: '400', fontSize: 14, color: colors.primary, marginVertical: 8}}>Linden Ave, Auburn, AL 36832</Text>
+      </View>
+      <View style={styles.checkInBtn}>
+        <CometButton onPress={() => navigation.navigate('Sign In')} >CHECK-IN</CometButton>
+      </View>
+      <Text style={{fontWeight: '400', fontSize: 14, color: colors.primary}}>Make sure to check-in by 3:30 pm!</Text>
     </View>
   )
 }
+const styles = StyleSheet.create({
+  mapImg: {
+    width: 300,
+    height: 150,
+    resizeMode: 'contain',
+    marginVertical: 30,
+  },
+  titleView: {
+    alignItems: 'center',
+    marginTop: 90
+  },
+  textView: {
+    marginVertical: 8,
+    alignItems: 'center'
+  },
+  checkInBtn: {
+    marginTop: 64,
+    marginBottom: 32,
+  }
+})
 export default AUMB_CheckIn
