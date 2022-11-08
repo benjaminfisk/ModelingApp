fb64bb70d9712a921fa42be98f9e02238ea399f7
Matthew Freestone <matthewfreestone1593@gmail.com>
2022-11-08 10:31:10 -0600

    scuffed solution for drawer

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
-import { View, Text, StyleSheet, Image, SafeAreaView, TouchableOpacity } from 'react-native'
+import { useState } from 'react'
+import { View, Text, StyleSheet, Image, SafeAreaView, TouchableOpacity, Modal } from 'react-native'
 import { colors } from '../assets/styles'
 import { LinearGradient } from 'expo-linear-gradient';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import { useNavigation } from '@react-navigation/native';
 
-import LogoutModal from './LogoutModal';
 import Feather from 'react-native-vector-icons/Feather';
 Feather.loadFont();
 import MusicIcon from '../assets/MusicIcon.png';
+import closeIcon from '../assets/closeIcon.png';
 
 type HeaderProps = React.ComponentProps<typeof View>;
 interface Props extends HeaderProps {
@@ -16,22 +17,10 @@ interface Props extends HeaderProps {
 
 const Header: React.FC<Props> = (props) => {
     const { title } = props
-    const Drawer = createDrawerNavigator();
-    //Drawer Navigation
-    const DrawerNavigator = () => {
-        return (
-            <Drawer.Navigator>
-                <Drawer.Screen
-                    name="Logout"
-                    component={LogoutModal}
-                />
-            </Drawer.Navigator>
-        )
-    }
+    const [drawerOpen, setDrawerOpen] = useState(false);
     return (
-        
+        <>
         <SafeAreaView style={styles.headerBG}>
-
             <View>
                 <LinearGradient colors={["#E57C45", "#EEAA86"]} style={styles.logoHolder}>
                     <Image source={MusicIcon} style={styles.logo} />
@@ -42,14 +31,40 @@ const Header: React.FC<Props> = (props) => {
             </View>
             <View style={styles.filler} />
             <View>
-                <TouchableOpacity >
+                <TouchableOpacity onPress={() => setDrawerOpen(true)}>
                     <Feather name="menu" size={40} color={colors.accent} style={styles.menuIcon} />
                 </TouchableOpacity>
             </View>
-            {/* <DrawerNavigator /> */}
         </SafeAreaView>
+        <Modal visible={drawerOpen} transparent={true} animationType="none">
+            <DrawerNavigator onClose={() => setDrawerOpen(false)}/>
+        </Modal>
+        </>
     )
 }
+
+type DrawerNavigatorProps = {
+    onClose: () => void;
+}
+const DrawerNavigator:React.FC<DrawerNavigatorProps> = ({onClose}) => {
+    const navigation = useNavigation<any>();
+    return (
+        <View style={drawerStyles.drawerBG}>
+            <View style={drawerStyles.titleRow}>
+                <Text style={{color: colors.black, fontSize: 16, fontWeight: "500"}}>Firstname L.</Text>
+                <TouchableOpacity onPress={onClose}>
+                    <Image source={closeIcon} style={drawerStyles.closeIcon} />
+                </TouchableOpacity>
+            </View>
+            <View style={drawerStyles.body}>
+                <TouchableOpacity onPress={() => navigation.navigate('Sign In')}>
+                    <Text style={drawerStyles.buttonText}>Log Out</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    )
+}
+
 
 const styles = StyleSheet.create({
     headerBG: {
@@ -87,4 +102,42 @@ const styles = StyleSheet.create({
     },
 })
 
+const drawerStyles = StyleSheet.create({
+    drawerBG: {
+        backgroundColor: colors.primary,
+        width: '50%',
+        height: 818,
+        position: 'absolute',
+        top: 0,
+        right: 0,
+        zIndex: 1,
+        borderColor: colors.secondary,
+        borderLeftWidth: 2,
+        borderBottomWidth: 2,
+    },
+    closeIcon: {
+        width: 24,
+        height: 24,
+    },
+    titleRow: {
+        backgroundColor: colors.secondary,
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'flex-end',
+        justifyContent: 'space-between',
+        paddingHorizontal: 16,
+        paddingVertical: 16,
+        height: 111,
+    },
+    body: {
+        backgroundColor: colors.accent,
+        flex: 1,
+    },
+    buttonText: {
+        color: colors.primary,
+        fontSize: 14,
+        fontWeight: '400',
+        margin: 16,
+    }
+})
 export default Header
